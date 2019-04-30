34159ec96e6fdaa56761aeade4398bd9201045f0
Raphael Arias <raphael.a.arias@outlook.com>
2019-04-30 22:26:36 +1000

    Add bundling function

---
+++ src/lib/bundle.js
@@ -0,0 +1,28 @@
+// Bundle compilation method
+
+import tmp from 'tmp';
+import path from 'path';
+import shell from 'shelljs';
+import winston from 'winston';
+
+export default function compileBundle() {
+  const workingDir = tmp.dirSync().name;
+
+  winston.info('Compiling application bundle');
+
+  // Generate Meteor build
+  winston.debug('generate meteor build');
+  shell.exec(`rm -rf ${workingDir}`);
+  shell.exec(`meteor build ${workingDir} --directory --server-only --architecture os.linux.x86_64`);
+
+  // Cleanup broken symlinks
+  winston.debug('checking for broken symlinks');
+  shell.find(path.join(workingDir, 'bundle')).forEach((symlinkPath) => {
+    // Matches symlinks that do not exist
+    if (shell.test('-L', symlinkPath) && !shell.test('-e', symlinkPath)) {
+      // Delete file
+      shell.rm('-f', symlinkPath);
+      winston.debug(`deleted symlink at '${symlinkPath}'`);
+    }
+  });
+}
