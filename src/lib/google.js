c733f908c9fd3e84f7a007db0251b5d80b8ccb54
Raphael Arias <raphael.a.arias@outlook.com>
2019-05-01 02:47:03 +1000

    Prevent newlines inside the JSON to break YAML

--- src/lib/google.js
+++ src/lib/google.js
@@ -29,7 +29,11 @@ export default class AppEngineInstance {
 
     // We add the Meteor settings now to avoid it being compiled to YAML
     const compactSettings = JSON.stringify(this.meteorSettings || {}, null, 0);
-    app = app.replace('METEOR_SETTINGS:', `METEOR_SETTINGS: ${compactSettings} \n`);
+    app = app
+      .replace('METEOR_SETTINGS:', `METEOR_SETTINGS: ${compactSettings} \n`)
+      .replace(/[^\x20-\x7E]/gmi, '');
+    // It will remove all non-printable characters.
+    // This are all characters NOT within the ASCII HEX space 0x20-0x7E.
 
     shell.exec(`echo '${app}' >${this.workingDir}/bundle/app.yaml`);
 
