a49f696f23486c43c5f4790df9f74dd8abf7c6e4
Raphael Arias <raphael.a.arias@outlook.com>
2019-05-01 22:03:29 +1000

    Update build

--- dist/lib/helpers.js
+++ dist/lib/helpers.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("core-js/modules/es.string.replace");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -15,34 +17,39 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Helper functions
 function initRepo() {
-  var path = '../node_modules/meteor-google-cloud/examples/';
+  var dirName = __dirname;
+  var path = dirName.replace('/dist/lib', '/examples/');
   var dockerFile = 'Dockerfile';
   var app = 'app.yml';
-  var settings = 'settings.json'; // Init app.yaml
+  var settings = 'settings.json';
+
+  _shelljs.default.exec('mkdir -p deploy');
+
+  var newFolderPath = `${process.cwd()}/deploy`; // Init app.yaml
 
   if (!_fs.default.existsSync('./deploy/app.yml')) {
-    _shelljs.default.cp('-R', `${path}${app}`, '.deploy/');
+    _shelljs.default.cp('-R', `${path}${app}`, newFolderPath);
 
-    _winston.default.info('app.yml created on .deploy/ it has the default/minimum settings you need');
+    _winston.default.info('app.yml created on deploy/ it has the default/minimum settings you need');
   } else {
-    _winston.default.info('app.yml already exists on .deploy/');
+    _winston.default.error('app.yml already exists on deploy/');
   } // Init Dockerfile
 
 
   if (!_fs.default.existsSync('./deploy/Dockerfile')) {
-    _shelljs.default.cp('-R', `${path}${dockerFile}`, '.deploy/');
+    _shelljs.default.cp('-R', `${path}${dockerFile}`, newFolderPath);
 
-    _winston.default.info('Dockerfile created on .deploy/');
+    _winston.default.info('Dockerfile created on deploy/');
   } else {
-    _winston.default.info('Dockerfile already exists on .deploy/');
+    _winston.default.error('Dockerfile already exists on deploy/');
   } // Init settings
 
 
   if (!_fs.default.existsSync('./deploy/settings.json')) {
-    _shelljs.default.cp('-R', `${path}${settings}`, '.deploy/');
+    _shelljs.default.cp('-R', `${path}${settings}`, newFolderPath);
 
-    _winston.default.info('Meteor settings file created on .deploy/');
+    _winston.default.info('Meteor settings file created on deploy/');
   } else {
-    _winston.default.info('settings.json already exists on .deploy/');
+    _winston.default.error('settings.json already exists on deploy/');
   }
 }
